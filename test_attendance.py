b83c6d09633b5192d496dbab16fa5b1f68b0542f
deffinru <ekaterinakopaeva3@gmail.com>
2026-09-22 14:00:08 +0300

    test: добавлен падающий тест для check_out

--- test_attendance.py
+++ test_attendance.py
@@ -5,6 +5,21 @@ import unittest
 from attendance import check_in, check_out
 
 
+class TestCheckIn(unittest.TestCase):
+    def setUp(self):
+        self.filename = "test_attendance.csv"
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+    def tearDown(self):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+    def test_check_in_creates_record(self):
+        """Проверяем, что check_in создаёт запись."""
+        check_in("Иван", self.filename)
+        self.assertTrue(os.path.exists(self.filename))
+
 class TestCheckOut(unittest.TestCase):
     def setUp(self):
         self.filename = "test_attendance.csv"
